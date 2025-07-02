e57b266c8d7d0785212de7e6401999d4db4d3a36
Kgahliso1193 <mogoshikgahliso@gmail.com>
2025-07-02 14:52:00 +0200

    Add files via upload

--- python.py
+++ python.py
@@ -1,8 +1,11 @@
-#Loopes
+#Loop Control statements
 
-#Using a while loop to count from  1 to 5
-count = 1
+count = 0
 
-while count <= 5:
+while count <5:
     print(count)
-    count += 1 # Increments the count by 1
+    count += 1
+    if count == 3:
+        break #Exits the loop when count is reached - 3
+    
+    

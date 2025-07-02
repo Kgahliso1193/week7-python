ab705bc34fad05a46e62f10d92b2481f73209372
Kgahliso1193 <mogoshikgahliso@gmail.com>
2025-07-02 14:48:59 +0200

    Add files via upload

--- python.py
+++ python.py
@@ -1,12 +1,11 @@
-#Control statements
+#Loopes
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+fruits = ["apple", "banana", "cherry"]
 
-if num1 > num2:
-    print(num1, "is greater than" , num2)
-elif num2 > num1:
-    print(num2, "is greater than", num1)
-else:
-    print("Both numbers are equal")   
-     
+for fruit in fruits:
+    print(fruit)
+    
+    
+    numbers = [1, 2, 3, 5, 7]
+    for number in numbers:
+        print(number)
